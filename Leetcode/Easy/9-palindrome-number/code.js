7cc86237e3833149d5d426536ac4890392ca3ef3
Leon Michalak <leonmichalak6@gmail.com>
2022-02-23 16:31:13 +0000

    Leetcode easy challenge done

--- Leetcode/Easy/9-palindrome-number/code.js
+++ Leetcode/Easy/9-palindrome-number/code.js
@@ -3,4 +3,23 @@
  * @return {boolean}
  */
 
-const isPalindrome = (x) => {};
+const isPalindrome = (x) => {
+  let num = Math.abs(x);
+  let reversedNum = 0;
+
+  let isPalindrome;
+  let lastDigit;
+
+  while (num != 0) {
+    lastDigit = num % 10;
+
+    reversedNum = reversedNum * 10 + lastDigit;
+    num = Math.floor(num / 10);
+  }
+
+  isPalindrome = x === reversedNum ? true : false;
+
+  return isPalindrome;
+};
+
+console.log(isPalindrome(121));
