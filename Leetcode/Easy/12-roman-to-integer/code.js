1c38aa7ed6d645bdf0639cbca52f5a75ec58bc02
Leon Michalak <leonmichalak6@gmail.com>
2022-03-14 18:10:25 +0000

    Leetcode easy solution

---
+++ Leetcode/Easy/12-roman-to-integer/code.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const romanToInt = (s) => {
+  symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  value = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    console.log(value);
+
+    symbols[s[i]] < symbols[s[i + 1]] ? (value -= symbols[s[i]]) : (value += symbols[s[i]]);
+  }
+
+  return value;
+};
+
+// console.log(romanToInt('III'));
+
+// console.log(romanToInt('LVIII'));
+
+console.log(romanToInt('MCMXCIV'));
