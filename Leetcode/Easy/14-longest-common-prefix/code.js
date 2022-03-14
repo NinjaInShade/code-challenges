de20f9c10631531a165b1dcd50e61065bb640456
Leon Michalak <leonmichalak6@gmail.com>
2022-03-14 19:37:51 +0000

    Leetcode easy solution

---
+++ Leetcode/Easy/14-longest-common-prefix/code.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+const longestCommonPrefix = (strs) => {
+  if (!strs.length) return '';
+
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[0][i] != strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+};
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
+
+console.log(longestCommonPrefix(['dog', 'racecar', 'car']));
+console.log(longestCommonPrefix(['', '']));
